2210a16ee2339a3979e7161d6584e036b75b2a88
Sambasivarao <samba.pendela@gmail.com>
2021-10-23 07:32:53 +0530

    code

---
+++ frontend/src/app/HomeModule/model/product.model.ts
@@ -0,0 +1,10 @@
+export interface Product{
+    "_id":string,
+    "name":string,
+    "brand":string,
+    "cost":number,
+    "rating":number,
+    "image":string,
+    "description":string,
+    "numReviews":number
+};
---
+++ frontend/src/app/HomeModule/services/product.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Product } from "../model/product.model";
+@Injectable({
+    providedIn:"root"
+})
+export class ProductsService{
+   constructor(private http:HttpClient){} 
+   public getProducts():Observable<Product[]>{
+       return this.http.get<Product[]>(`http://localhost:8080/api/products`);
+   }
+}
